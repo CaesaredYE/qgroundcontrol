e55872b29023d605c653645ff667d2018a8a7a8a
yebinbin <yebinbin0127@gmail.com>
2025-05-11 23:17:31 +0800

    fix

--- custom/src/RadarReceiver.cc
+++ custom/src/RadarReceiver.cc
@@ -21,39 +21,53 @@ void RadarReceiver::startConnection(const QString& remoteIp, quint16 remotePort,
     const uint8_t data[10] = {0xAA,0xAA,0xAA,0xAA,0xAA,0xAA,0xAA,0xAA,0xAA,0xAA};
     QByteArray byteArray(reinterpret_cast<const char*>(data), sizeof(data));
 
-
+    _udpSocket->writeDatagram(byteArray, _remoteHost, _remotePort);
+ 
     QTimer* pollTimer = new QTimer(this);
     connect(pollTimer, &QTimer::timeout, this, [=]() {
-        qint64 sent = _udpSocket->writeDatagram(byteArray, _remoteHost, _remotePort);
-        if (sent == -1) {
-            qWarning() << "[UDP] Send failed:" << _udpSocket->errorString();
-        }
+        _udpSocket->writeDatagram(byteArray, _remoteHost, _remotePort);
     });
-    pollTimer->start(1000); 
+    pollTimer->start(1000 * 60);
 }
+
+struct TrackInfo {
+    uint32_t batch; // 批号
+    float lat; // 纬度
+    float lon; // 经度
+    float alt; // 高度
+    uint16_t existFlag; // 存在标识
+};
 
 void RadarReceiver::processPendingDatagrams() {
     // QList<QGeoCoordinate> targets;
-    qDebug() << "Datagram received!";
 
     while (_udpSocket->hasPendingDatagrams()) {
         QByteArray datagram;
         datagram.resize(_udpSocket->pendingDatagramSize());
-        QHostAddress sender;
-        quint16 senderPort;
-        qint64 bytesRead = _udpSocket->readDatagram(datagram.data(), datagram.size(), &sender, &senderPort);
-
+        qint64 bytesRead = _udpSocket->readDatagram(datagram.data(), datagram.size());
 
         if (bytesRead == -1) {
             qDebug() << "读取失败：" << _udpSocket->errorString();
-        } else {
-            qDebug() << "收到数据：" << datagram.toHex();
         }
 
-        if (sender != _remoteHost || senderPort != _remotePort) continue;
-        
-        // QDataStream stream(datagram);
-        // stream.setByteOrder(QDataStream::LittleEndian);
+        // 航迹报文
+        if (datagram.size() >= 56 && datagram.constData()[4] == 0x01) {
+            qDebug() << "收到航迹报文：" << datagram.toHex();
+
+            TrackInfo info {};
+            info.batch = datagram.constData()[7];
+            memcpy(&info.lat, datagram.constData() + 24, sizeof(float));
+            memcpy(&info.lon, datagram.constData() + 28, sizeof(float));
+            info.existFlag = datagram.constData()[36];
+            memcpy(&info.alt, datagram.constData() + 52, sizeof(float));
+
+            qDebug() << "批号" << info.batch;
+            qDebug() << "纬度" << info.lat;
+            qDebug() << "经度" << info.lon;
+            qDebug() << "高度" << info.alt;
+            qDebug() << "存在标识" << info.existFlag;
+        }
+
         // float angle, distance, intensity;
         // stream >> angle >> distance >> intensity;
 
@@ -70,7 +84,6 @@ void RadarReceiver::processPendingDatagrams() {
         //     origin.longitude() + qRadiansToDegrees(deltaLon)
         // );
         // targets.append(targetCoord);
-        
     }
 
     // if (!targets.isEmpty()) {
