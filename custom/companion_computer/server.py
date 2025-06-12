ace14c48f3a56c7d2650c17560951c00bf786f89
yebinbin <yebinbin0127@gmail.com>
2025-06-12 19:37:18 +0800

    feat

--- custom/companion_computer/server.py
+++ custom/companion_computer/server.py
@@ -2,9 +2,17 @@ import socket
 import json
 import subprocess
 import os
+import logging
+import threading
 
 UDP_IP = "0.0.0.0"
 UDP_PORT = 6000
+
+logging.basicConfig(
+    filename='udpserver_error.log',
+    level=logging.ERROR,
+    format='%(asctime)s [%(levelname)s] %(message)s'
+)
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
@@ -12,32 +20,56 @@ sock.bind((UDP_IP, UDP_PORT))
 
 print(f"Listening on {UDP_PORT}...")
 
+def run_script_realtime(cmd, script_path, addr):
+    try:
+        with subprocess.Popen(
+            ["bash", script_path],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            bufsize=1,
+            text=True
+        ) as proc:
+            for line in proc.stdout:
+                sock.sendto(json.dumps({
+                    "cmd": cmd,
+                    "output": line.strip()
+                }).encode(), addr)
+
+            returncode = proc.wait()
+            sock.sendto(json.dumps({
+                "cmd": cmd,
+                "status": "done",
+                "exit_code": returncode
+            }).encode(), addr)
+
+    except Exception as e:
+        logging.error(f"[ERROR]{cmd}.sh -> {e}")
+
+
 while True:
     data, addr = sock.recvfrom(1024)
     print(data, addr)
+    
+    cmd = None
     try:
         msg = json.loads(data.decode())
         if msg.get("type") == "exec":
             cmd = msg.get("cmd")
 
-            if cmd == 'start_ros':
-                bash = './start_ros.sh'
-            elif cmd == 'detect':
-                bash = './detect.sh'
-            else:
-                bash = None
-                output = f"Unknown cmd: {cmd}"
+            if not cmd:
+                continue
 
-            if bash:
-                result = subprocess.run(f"bash {bash}", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, timeout=5)
-                output = (result.stdout + result.stderr).decode()
+            script_file = f"./{cmd}.sh"
 
-            response = {
-                "cmd": cmd,
-                "output": output
-            }
+            if not os.path.isfile(script_file):
+                logging.error(f"找不到文件: {script_file}")
+                continue
 
-            # 回传结果
-            sock.sendto(json.dumps(response).encode(), addr)
+            threading.Thread(
+                target=run_script_realtime,
+                args=(cmd, script_file, addr),
+                daemon=True  # 设置为守护线程，主线程退出时它也会结束
+            ).start()
+
     except Exception as e:
-        print(f"Error: {e}")
+        logging.error(f"[ERROR]{cmd}.sh -> {e}")
